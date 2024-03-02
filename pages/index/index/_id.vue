7867cfeeeeaed0a2f9b54742d4bc7771ccb83967
Nathan Bienvenu <bienvenu.nathan72@gmail.com>
2024-03-02 19:24:36 +0100

    refactor tagBar

---
+++ pages/index/index/_id.vue
@@ -0,0 +1,145 @@
+<template>
+    <nav class="tag-bar">
+        <UIButton class="tag-bar-close-button" buttonType="button" iconName="mi:close" @is-clicked="closeTagBar"/>
+        <form class="tag-bar-form">
+        <div class="tag-bar-content">
+            <div> 
+                <UIInput id="name" type="text" 
+                :has-label="true"/>
+                <div class="tag-bar-category">
+                    <UIInput class="tag-bar-type" 
+                    id="type" type="text" 
+                    :has-label="true"/>
+                    <UIInput class="tag-bar-extension" 
+                    id="extension" type="text" 
+                    :has-label="true"/>
+                </div>
+                <div class="tag-bar-category">
+                    <UIInput class="tag-bar-created" 
+                    id="created" type="text" 
+                    :has-label="true"/>
+                    <UIInput class="tag-bar-modified" 
+                    id="modified" type="text" 
+                    :has-label="true"/>
+                </div>
+                <UIInput id="path" type="text" 
+                :has-label="true"/>
+            </div>
+            <hr>
+            <div class="tag-bar-add">
+                <UIButton class="tag-bar-add-button" 
+                buttonType="bar" 
+                iconName="gala:add" 
+                buttonName="Add a tag"/>
+                <div class="tag-bar-add-divider"/>
+            </div>
+            <hr>
+            <UIInput :isComment="true" id="comments" 
+            :has-label="true"/>
+        </div>
+        <div class="tag-bar-bottom">
+            <input class="tag-button-submit" type="submit" value="Submit">
+        </div>
+        </form>
+    </nav>
+</template>
+
+<script lang="ts" setup>
+
+    function closeTagBar(): void {
+        const tagBar = document.getElementsByClassName('tag-bar')[0] as HTMLInputElement | null
+        const tagForm = document.getElementsByClassName('tag-bar-form')[0] as HTMLInputElement | null
+        const tagBarCloseButton = document.getElementsByClassName('tag-bar-close-button')[0] as HTMLElement | null
+        let tagBarIsClosed: boolean = false
+        
+        if (tagBar != null && tagForm != null && tagBarCloseButton != null) {
+            if (tagBarIsClosed) {
+                tagBar.style.width = "250px"
+                tagForm.style.display = "flex"
+                tagBarCloseButton.style.display = "flex"
+                tagBarIsClosed = false
+            } else {
+                tagBar.style.width = "0px"
+                tagForm.style.display = "none"
+                tagBarCloseButton.style.display = "none"
+                tagBarIsClosed = true
+            }
+        }
+    }
+
+</script>
+
+<style lang="scss" scoped>
+@import './assets/scss/tools.scss';
+.tag-bar {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    flex-grow: 0;
+    flex-shrink: 0;
+    box-shadow: rgba(0, 0, 0, 0.025) -1px 0px 0px 0px inset;
+    transition: 0.1s ease-in-out;
+
+    @media (prefers-color-scheme: dark) {
+        background-color: color(dark_secondary);
+        color: color(dark_unselectedTypography);
+    }
+
+    @media (prefers-color-scheme: light) {
+        background-color: color(light_secondary);
+        color: color(light_unselectedTypography);
+    }
+    
+    &-close-button {
+        position: absolute;
+        top: 5px; left: 2px;
+        margin-bottom: 10px;
+    }
+    
+    &-content {
+        padding: 15px;
+        margin-top: 20px;
+    }
+    
+    &-type, &-created {
+        margin-right: 5px;
+    }
+    
+    &-extension, &-modified {
+        margin-left: 5px;
+    }
+    
+    &-add {
+        display: flex;
+        margin: 15px 0;
+        
+        &-button {
+            white-space: nowrap;
+        }
+        
+        &-divider {
+            width: 100%;
+            border-bottom: 1px solid white;
+            margin: 10px 0 10px 10px;
+            
+        }
+    }
+    
+    &-category {
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    &-bottom {
+        position: absolute;
+        bottom: 0; left: 0;
+        display: flex; 
+        align-content: center; 
+        justify-content: center;
+        background-color: aquamarine;
+        height: 80px;
+        width: 100%;
+    }
+}
+</style>
